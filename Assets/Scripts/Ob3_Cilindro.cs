9f9a8381641cc09adf8440e12252d22d87d5e9a9
AdrianDiazOlias <adriart3.0@gmail.com>
2025-11-21 03:48:00 +0100

    Crecimiento del cilindro
    Añadido en el script del cilindro que modifique su tamaño en el eje Y

--- Assets/Scripts/Ob3_Cilindro.cs
+++ Assets/Scripts/Ob3_Cilindro.cs
@@ -2,18 +2,40 @@ using UnityEngine;
 
 public class Ob3_Cilindro : MonoBehaviour
 {
+    public float vCrecimiento;
+
+    private Transform trans;
+    private float maxHeight = 2f;
+    private float minHeight = 0.25f;
+    private bool isGrowing = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        trans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // modify a copy of the Vector3 and assign it back to avoid modifying the return value of localScale
+        Vector3 scale = trans.localScale;
 
+        if (isGrowing)
+        {
+            scale.y += 0.01f * vCrecimiento;
+            if (scale.y >= maxHeight)
+                isGrowing = false;
+        }
+        else
+        {
+            scale.y -= 0.01f * vCrecimiento;
+            if (scale.y <= minHeight)
+                isGrowing = true;
+        }
+
+        trans.localScale = scale;
     }
-
 
     void OnEnable()
     {
